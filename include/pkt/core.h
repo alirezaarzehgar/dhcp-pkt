7f3240c59def95dcbb41317b69daf21cd7fb330e
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-09-28 14:53:23 +0330

    init

---
+++ include/pkt/core.h
@@ -0,0 +1,10 @@
+#include "dhcp.h"
+#include "generate.h"
+#include "analyze.h"
+
+#if !defined(PKT_CORE_H)
+#define PKT_CORE_H
+
+
+
+#endif
---
+++ tests/pkt/include/pkt/generation_test.h
@@ -0,0 +1,14 @@
+#if !defined(PKT_GENERATION_TEST)
+#define PKT_GENERATION_TEST
+
+#include "analyze_test.h"
+
+void packetGenMainTest();
+
+void pktGenOfferTest();
+
+void pktGenAckTest();
+
+void pktGenNakTest();
+
+#endif
